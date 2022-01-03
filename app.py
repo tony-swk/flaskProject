21998c8e81a529604374d8b8b9a9f43402ffb457
tony-swk <tony@spacewalk.tech>
2022-01-03 15:59:16 +0900

    flask test second commit with html rendering

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def hello_world():
-    return 'SPACEWALK Home'
+    return '''
+    <h1>이건 h1 제목</h1>
+    <p>이건 p 본문</p>
+    <a href="https://www.naver.com">Naver 홈페이지 바로가기</a>
+    '''
 
 
-@app.route('/user')
-def user():
-    return 'Hello User, this is user page of  SPACEWALK'
+@app.route('/user/<user_name>/<int:user_id>')
+def user(user_name, user_id):
+    return f'Hello, {user_name} ({user_id})'
 
 
 if __name__ == '__main__':
