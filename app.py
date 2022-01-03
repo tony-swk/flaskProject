10493f4709eb6d111bc964ecce25e7dad6777182
tony-swk <tony@spacewalk.tech>
2022-01-03 17:15:31 +0900

    Flask web test with dummy score checking service

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+student_data = {
+    1: {"name": "슈퍼맨", "score": {"국어": 90, "수학": 65}},
+    2: {"name": "배트맨", "score": {"국어": 75, "영어": 80, "수학": 75}}
+}
+
 
 @app.route('/')
-@app.route('/home')
-def hello_world():
-    return '''
-    <h1>이건 h1 제목</h1>
-    <p>이건 p 본문</p>
-    <a href="https://www.naver.com">Naver 홈페이지 바로가기</a>
-    '''
+def index():
+    return render_template("index.html", template_students=student_data)
 
 
-@app.route('/user/<user_name>/<int:user_id>')
-def user(user_name, user_id):
-    return f'Hello, {user_name} ({user_id})'
+@app.route("/student/<int:std_id>")
+def student(std_id):
+    return render_template("student.html",
+                           template_name=student_data[std_id]["name"],
+                           template_score=student_data[std_id]["score"])
 
 
 if __name__ == '__main__':
