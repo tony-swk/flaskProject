d017253c3731d7c7cecedde424fb551f72c61f53
tony-swk <tony@spacewalk.tech>
2022-01-04 10:53:18 +0900

    test commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 student_data = {
-    1: {"name": "슈퍼맨", "score": {"국어": 90, "수학": 65}},
+    1: {"name": "슈퍼맨", "score": {"국어": 90, "수학": 63}},
     2: {"name": "배트맨", "score": {"국어": 75, "영어": 80, "수학": 75}}
 }
 
